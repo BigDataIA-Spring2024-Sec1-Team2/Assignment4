9b174ea854ab71571deb867a849cc020f8450a39
Sayali Dalvi <dalvi.sa@northeastern.edu>
2024-03-22 10:34:13 -0400

    Created first fastapi api

---
+++ fastapi/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import service, aws_service
+
+app = FastAPI()
+app.include_router(service.router, prefix="/api")
+app.include_router(aws_service.router, prefix="/aws")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
---
+++ fastapi/routers/aws_service.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, HTTPException
+from dotenv import dotenv_values
+from fastapi import FastAPI, File, UploadFile
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
+import logging
+from utils.util import generate_file_name, is_pdf
+
+router = APIRouter()
+
+config = dotenv_values(".env")
+
+# Create an S3 client
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=config["S3_ACCESS_KEY"],
+    aws_secret_access_key=config["S3_SECRET_KEY"],
+    region_name=config["S3_REGION"]
+)
+
+@router.post("/upload/", tags=["aws"])
+async def upload_file(file_obj: UploadFile = File(...)):
+    if not is_pdf(file_obj.filename):
+        raise HTTPException(status_code=400, detail="Only PDF files are allowed")
+    try:
+        # Upload the file to S3
+        S3_BUCKET_NAME = config["S3_BUCKET_NAME"]
+        folder_name = config["S3_UPLOAD_PDF_FOLDER"]
+        file_name = generate_file_name()
+        print("filename = ", file_name)
+        success = upload_file_to_bucket(file_obj.file, S3_BUCKET_NAME,folder_name, file_name)
+        print("Uploaded ", file_name, " to S3 bucket")
+        if success:
+            return {"message": "File uploaded successfully"}
+        else:
+            return {"error": "Some error occurred"}
+    except NoCredentialsError:
+        return {"error": "AWS credentials not found or invalid"}
+    except Exception as e:
+        return {"error": str(e)}
+
+def upload_file_to_bucket( file_obj, bucket, folder, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_obj: File to upload
+    :param bucket: Bucket to upload to
+    :param folder: Folder to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_obj
+
+    # Upload the file
+    try:
+        response = s3_client.upload_fileobj(file_obj, bucket, f"{folder}/{object_name}")
+    except ClientError as e:
+        logging.error(e)
+        return False
+    except NoCredentialsError:
+        logging.error(e)
+        return False
+    except Exception as e:
+        logging.error(e)
+        return False
+    return True
